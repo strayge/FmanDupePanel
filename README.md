33d33166f94832500db2b6b3d4981d8d2fb82706
Sergey O <strayge@gmail.com>
2021-07-25 19:00:41 +0300

    release plugin

---
+++ README.md
@@ -0,0 +1,9 @@
+## DupePanel
+
+Plugin for [fman.io](https://fman.io) to open same paths at oppoite panel.
+
+Install with [fman's built-in command for installing plugins](https://fman.io/docs/installing-plugins).
+
+### Usage
+
+`<Ctrl>-d` by default. Or, use `dupe panel` from the command pallete.
---
+++ dupepanel/__init__.py
@@ -0,0 +1,34 @@
+from fman import DirectoryPaneCommand
+
+
+class DupePanel(DirectoryPaneCommand):
+    def __call__(self):
+        panes = self.pane.window.get_panes()
+
+        currentPane = self.pane
+        lpane = panes[0]
+        lpane_path = lpane.get_path()
+        lpane_selection = lpane.get_selected_files()
+        lpane_cursor = lpane.get_file_under_cursor()
+
+        rpane = panes[1]
+        rpane_path = rpane.get_path()
+        rpane_selection = rpane.get_selected_files()
+        rpane_cursor = rpane.get_file_under_cursor()
+
+        def _setLeftPaneSelections():
+            if lpane_cursor is not None:
+                rpane.place_cursor_at(lpane_cursor)
+            for url in lpane_selection:
+                rpane.toggle_selection(url)
+
+        def _setRightPaneSelections():
+            if rpane_cursor is not None:
+                lpane.place_cursor_at(rpane_cursor)
+            for url in rpane_selection:
+                lpane.toggle_selection(url)
+
+        if panes[0] == currentPane:
+            rpane.set_path(lpane_path, _setLeftPaneSelections)
+        else:
+            lpane.set_path(rpane_path, _setRightPaneSelections)
